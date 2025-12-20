0a9ee2c7f8a6b67fbb4c5694b030cb0e15d2a77e
Kazuki Tobita <kazukiti201@gmail.com>
2025-12-21 02:10:41 +0900

    Add camera

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,23 @@
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <h1>face-controller</h1>
+  <video id="webcam" autoplay playsinline style="transform: scaleX(-1);"></video>
 `
+
+async function setupCamera() {
+  const video = document.querySelector<HTMLVideoElement>('#webcam')!
+
+  let stream
+  try {
+    stream = await navigator.mediaDevices.getUserMedia({
+      video: { width: 640, height: 480 }
+    })
+  } catch (error) {
+    console.error('Error accessing camera:', error)
+    return
+  }
+
+  video.srcObject = stream
+  console.log('Camera access granted')
+}
+
+setupCamera()
